863ed8426dbfe91a865cf2bf6cbdad0bfd0c82a7
Gajendra <chilhategajendra@gmail.com>
2024-01-04 11:34:59 +0530

    currency converter

--- src/App.js
+++ src/App.js
@@ -2,56 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './Home';
 import { useCallback, useEffect, useRef, useState } from 'react';
+import Card from './component/Card';
+
 
 function App() {
-  const [length,setlength]=useState(8);
-  const [num,setnum]=useState(false);
-  const [char,setchar]=useState(false);
-  const [pas,setpas]=useState('');
+  const [amt,setamt]=useState(1);
+    const [cur,setcur]=useState('usd');
+    const [to,setto]=useState(null);
+    const [tocur,settocur]=useState('inr');
+    const [final,setfinal]=useState(null);
 
-  //useRef hook
-  const pasref=useRef(null)
-
-  const pasgen=()=>{
-     let str="ABCDEFGabccdefg";
-     if(num)
-     str+="0123456789";
-    if(char)
-    str+="!@#$%^&*()";
-    let pass="";
-    for(let i=1;i<=length;i++)
-    {
-      let c=Math.floor(Math.random()*str.length+1)
-      pass+=str.charAt(c);
-    }
-    setpas(pass);
+  const [res,setres]=useState([])
+  
+  const fetchdata= async()=>{
+    let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${cur}.json`;
+    let data= await fetch(url);
+    data=await data.json();
+    setres(data[cur])
+    console.log(res);
+   setfinal(amt*res[tocur])
+  //  console.log(cur)
+  //  console.log(tocur)
+  //  console.log(amt)
+  //  console.log(final)
   };
  
-   const copyfun=()=>{
-    pasref.current?.select();
-      window.navigator.clipboard.writeText(pas);
-   }
-
-  const calpas=()=>{
-    pasgen();
-  }
-  useEffect(()=>{
-    pasgen();
-  },[length,num,char,setpas])
-  
   return (
     <div>
-      <h1 style={{textAlign:'center'}}>Password Generator</h1>
-     <div style={{textAlign:'center', borderRadius:'10px', marginTop:'100px', backgroundColor:'olive', padding:'30px', width:'40%', justifyContent:'center', marginLeft:'28%'}}>&nbsp;&nbsp;
-         <input  type="text" value={pas} placeholder='Password' ref={pasref} onChange={(e)=>{setpas(e.target.value)}}/>
-         <button style={{backgroundColor:'blue',borderRadius:'10px'}} onClick={copyfun} >COPY</button><br/><br/>
-         <h3>Enter the Length : </h3>
-         <input  style={{width:'40px', textAlign:'center'}} type="text" value={length} onChange={(e)=>{setlength(e.target.value)}}/><br/><br/>
-         <input type="checkbox" defaultChecked={num} onChange={()=>{setnum(!num)}}/><label htmlFor="">Numbers</label>&nbsp;
-         <input type="checkbox" defaultChecked={char} onChange={()=>{setchar(!char)}}/><label htmlFor="">Characters</label>&nbsp;
-         <button onClick={calpas}>Click to generate</button>
-         
-     </div>
+      <h1 style={{textAlign:'center'}}>Currency Converter</h1>
+      <div style={{width:'50%',height:'50%', backgroundColor:'olive', padding:'60px', marginLeft:'250px', marginTop:'50px'}}>
+            <Card label="From" amt={amt} amtch={(e)=>{setamt(e.target.value)}} cur={cur} curch={(e)=>{setcur(e.target.value)}}/><br/><br/>
+            <Card label="To" amt={final} cur={tocur} curch={(e)=>{settocur(e.target.value)}} />
+            <button onClick={fetchdata} style={{borderRadius:'5px',backgroundColor:'blue',marginTop:'20px',fontSize:'40px',marginLeft:'230px'}}> Exchange</button>
+      </div>
      
     </div>
   );
